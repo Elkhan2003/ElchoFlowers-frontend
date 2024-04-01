f739ba1ef40306ea9fac9b93b860788368d83ec9
Elcho911 <boss.armsport@gmail.com>
2024-04-01 12:48:10 +0600

    test

--- src/components/framerMotion/AnimatedNumbers.tsx
+++ src/components/framerMotion/AnimatedNumbers.tsx
@@ -1,38 +1,37 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-"use client";
-import React, { FC, useEffect, useRef } from "react";
+import { FC, useEffect, useRef } from "react";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
 
 interface AnimatedNumbersProps {
-  value: number;
+	value: number;
 }
 
 const AnimatedNumbers: FC<AnimatedNumbersProps> = ({ value }) => {
-  const ref: any = useRef(null);
+	const ref: any = useRef(null);
 
-  const motionValue: any = useMotionValue(0);
-  const springValue: any = useSpring(motionValue, { duration: 7500 });
-  const isView: boolean = useInView(ref);
+	const motionValue: any = useMotionValue(0);
+	const springValue: any = useSpring(motionValue, { duration: 7500 });
+	const isView: boolean = useInView(ref);
 
-  useEffect(() => {
-    if (isView) {
-      motionValue.set(value);
-    }
-  }, [isView, value, motionValue]);
+	useEffect(() => {
+		if (isView) {
+			motionValue.set(value);
+		}
+	}, [isView, value, motionValue]);
 
-  useEffect(() => {
-    springValue.on("change", (latest: any): void => {
-      // console.log(latest);
-      if (ref.current && latest.toFixed(0) <= value) {
-        ref.current.textContent = latest.toFixed(0);
-      }
-    });
-  }, [springValue, value]);
+	useEffect(() => {
+		springValue.on("change", (latest: any): void => {
+			// console.log(latest);
+			if (ref.current && latest.toFixed(0) <= value) {
+				ref.current.textContent = latest.toFixed(0);
+			}
+		});
+	}, [springValue, value]);
 
-  return (
-    <>
-      <span ref={ref}></span>
-    </>
-  );
+	return (
+		<>
+			<span ref={ref}></span>
+		</>
+	);
 };
 export default AnimatedNumbers;
